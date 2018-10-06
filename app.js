191823eeb6bd11b86f60a7374677ffe58b3240fa
Felix Wu <mail@flxwu.com>
2018-10-06 23:58:28 +0200

    whole express app setup

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const admin = require('firebase-admin');
+const octokit = require('@octokit/rest')();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth');
+const initConfigs = require('./util/config');
+initConfigs(admin);
+const database = admin.database();
 
-var app = express();
+const router = require('./routes/api')(admin.database(), octokit);
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,23 +20,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'client/public')));
 
-app.use('/api/auth', authRouter);
-app.use('/api/users', usersRouter);
+app.use('/api', router);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.send(err);
 });
 
 module.exports = app;
