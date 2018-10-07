eece935e925ac2e5ac8b349cae938ab57f550eca
Felix Wu <mail@flxwu.com>
2018-10-07 13:07:39 +0200

    init frontend

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'client/public')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.use('/api', router);
 
