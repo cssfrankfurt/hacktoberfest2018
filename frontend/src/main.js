b211942210cf64531fff2d46061c31f7c30974eb
Felix Wu <mail@flxwu.com>
2018-10-07 14:22:40 +0200

    lint

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import io from 'socket.io-client';
+import io from "socket.io-client";
 
 Vue.config.productionTip = false;
 
-const socket = io("http://localhost:5000/")
+const socket = io("http://localhost:5000/");
 
 new Vue({
   router,
