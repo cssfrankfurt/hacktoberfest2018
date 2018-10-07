9aac6470eb4d49cbe70f673d399a72b470f93049
Felix Wu <mail@flxwu.com>
2018-10-07 14:21:25 +0200

    add socket-io client on frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,11 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import io from 'socket.io-client';
 
 Vue.config.productionTip = false;
+
+const socket = io("http://localhost:5000/")
 
 new Vue({
   router,
