0e370f21b58fb26fa5e92d966dba0de3a3b4dc72
vicbergquist <victoria.bergquist@gmail.com>
2018-10-08 14:04:07 +0200

    added loading logic

--- frontend/src/store/modules/api.js
+++ frontend/src/store/modules/api.js
@@ -21,12 +21,14 @@ const actions = {
     try {
       const response = await axios.get("/api/data");
       commit("RECEIVE_USERS", { data: response.data });
+      commit("loader/setLoading", false, { root: true });
     } catch (error) {
       console.error(error); // eslint-disable-line no-console
     }
   },
   UPDATE_USERS({ commit }, updatedData) {
     commit("RECEIVE_USERS", { data: updatedData });
+    commit("loader/setLoading", false, { root: true });
   }
 };
 
