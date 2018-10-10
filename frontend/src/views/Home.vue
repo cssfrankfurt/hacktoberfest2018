01ec5c3c5cf399d8235a592fdfb675b022f58148
vicbergquist <victoria.bergquist@gmail.com>
2018-10-10 15:22:40 +0200

    landing page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,27 +1,152 @@
 <template>
-  <div class="page">
-    <!-- TODO: Rewrite text, make button better. Update with find out more link. -->
-    <img 
-      class="hacktoberfest" 
-      src="/hacktoberfest.svg" 
-      alt="Hacktoberfest Frankfurt">
-    <div class="content white--text">
-      <h1>Welcome to Hacktoberfest Frankfurt 2018!</h1>
-      <p>We are excited that you've joined us to celebrate Open Source with the Frankfurt and Rhein-Main tech community! Take a look around to see what you can expect from this event!</p>
-      <p>Join our live <router-link 
-        class="white--text" 
-        to="/leaderboard">leaderboard</router-link> to track your progress and see how our community is doing.</p>
-      <v-btn 
-        class="accent white--text" 
-        @click="login">Join us</v-btn> or find out more here.
+  <section class="section section-home">
+    <div class="main-img">
+      <img 
+        class="hacktoberfest-img" 
+        src="/hacktoberfest.svg" 
+        alt="Hacktoberfest Frankfurt">
     </div>
-  </div>
+    <div class="stats">
+      <ul class="list list-stats">
+        <li 
+          class="list-item"
+          v-for="stat in stats" 
+          :key="stat.index">
+          <div class="stat-number">{{stat.number}}</div>
+          <div class="stat-name">{{stat.name}}</div>
+        </li>
+      </ul>
+    </div>
+    <article class="article article-community">
+      <list-component class="community-list" :items="communities" :itemType="'community'"/>
+      <h2 class="sponsor-heading">Sponsored by</h2>
+      <list-component class="sponsor-list" :items="sponsors" :itemType="'sponsor'"/>
+    </article>
+  </section>
 </template>
 
 <script>
+import ListComponent from "@/components/ListComponent"
 import { mapActions } from "vuex";
 export default {
   name: "Home",
+  components: {
+    ListComponent
+  },
+  data: () => ({
+    sponsors: [
+      {
+        name: "melsicon",
+        url: "https://melsicon.de",
+        img: '/melsicon.svg'
+      },
+      {
+        name: "rocketloop",
+        url: 'https://rocketloop.de',
+        img: './sponsors/rocketloop.svg'
+      },
+      {
+        name: "mindspace",
+        url: "https://mindspace.me",
+        img: './sponsors/mindspace.svg'
+      },
+      {
+        name: "msg",
+        url: 'https://msg.group',
+        img: './sponsors/msg.svg'
+      },
+      {
+        name: "codemonauts",
+        url: "https://codemonauts.com",
+        img: './sponsors/codemonauts.svg'
+      },
+      {
+        name: "uib GmbH",
+        url: "https://uib.de",
+        img: './sponsors/uib.svg'
+      },
+      {
+        name: "DigitalOcean",
+        url: "https://digitalocean.com",
+        img: './sponsors/digitalocean.svg'
+      },
+      {
+        name: "GitHub",
+        url: "https://github.com",
+        img: './sponsors/github.svg'
+      },
+      {
+        name: "Twilio",
+        url: "https://twilio.com",
+        img: './sponsors/twilio.svg'
+      }
+    ],
+    communities: [
+      {
+        name: "Angular Frankfurt",
+        url: "https://www.meetup.com/Angular-Frankfurt/"
+      },
+      {
+        name: "ASP.NET Core Rhein Main",
+        url: "https://www.meetup.com/NET-Core-Rhein-Main/"
+      },
+      {
+        name: "Code Review",
+        url: "https://www.meetup.com/code-review/"
+      },
+      {
+        name: "CSS Frankfurt",
+        url: "https://cssfrankfurt.de"
+      },
+      {
+        name: "Elixir Meetup Rhein Main",
+        url: "https://www.meetup.com/Elixir-Meetup-Rhein-Main/"
+      },
+      {
+        name: "Frankfurt Data Science",
+        url: "https://www.meetup.com/FrankfurtDataScience/"
+      },
+      {
+        name: "Frontend RheinMain",
+        url: "https://www.meetup.com/frontend_rm/"
+      },
+      {
+        name: "GDG Rhein Main",
+        url: "https://www.meetup.com/gdgrheinmain/"
+      },
+      {
+        name: "Vue.js Frankfurt",
+        url: "https://meetup.com/vuejsfrankfurt"
+      },
+      {
+        name: "Techettes Frankfurt",
+        url: "https://www.meetup.com/Techettes/"
+      },
+      {
+        name: "Women Techmakers Rhein-Main",
+        url: "https://www.meetup.com/Women-Techmakers-Frankfurt_Rhein-Main/"
+      }
+    ]
+  }),
+  computed: {
+    stats(){
+      const statsArray = [
+      {
+        name: "Communities",
+        number: '10+'
+      },
+      {
+        name: "Participants",
+        number: '140+'
+      },
+      {
+        name: "Sponsors",
+        number: this.sponsors.length
+      }
+    ]
+    return statsArray
+    },
+  },
   methods: {
     ...mapActions({
       login: "login/login"
@@ -31,7 +156,38 @@ export default {
 </script>
 
 <style lang="sass">
-  .hacktoberfest
+
+  .section-home
+    min-height: 100vh
+
+  .main-img
     max-width: 600px
-    padding: 1rem
+    text-align: center
+    margin: 1rem auto
+
+  .hacktoberfest-img
+    width: 100%
+
+  .stats
+    flex-basis: 100%
+    text-align: center
+    padding-bottom: .5rem
+
+  .list.list-stats
+    max-width: 700px
+    margin: auto
+    justify-content: space-between
+
+  .stat-number
+    font-size: 2.5rem
+    font-weight: 700
+
+  .stat-name, .sponsor-heading
+    text-align: center
+    text-transform: uppercase
+    font-size: 1rem
+    font-weight: 700
+    letter-spacing: 1px
+    opacity: .7
+
 </style>
