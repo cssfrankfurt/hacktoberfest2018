1167d769596cf8304fa18af91bf982f5151526ed
vicbergquist <victoria.bergquist@gmail.com>
2018-10-12 13:38:14 +0200

    reporting guidelines

---
+++ frontend/src/views/ReportGuideline.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="section section-coc">
+    <img 
+      src="/hacktoberfest_title.svg" 
+      alt="hacktoberfest" 
+      class="leaderboard-header">
+    <article class="article article-coc">
+      <h1 class="coc-heading" id="reporting-guidelines">Reporting Guidelines</h1>
+      <p>If you are subject to or witness unacceptable behavior, or have any other concerns, please contact us at <strong>hello@hacktoberfestffm.de</strong></p>
+      <h2 class="coc-sub-heading" id="_1-information-we-need-from-you">1. Information we need from you</h2>
+      <p>Please include the following information (English or German) in your report:</p>
+      <ul class="coc-list">
+        <li><strong>CoC Report [DD.MM.YYYY]</strong> as the subject headline (fill out the current date)</li>
+        <li>Your contact information (so we can follow up on your report)</li> 
+        <li>Names (real/nicknames/pseudonyms) of any individuals involved and possible witness names</li>
+        <li>When and where the incident happened (please be as specific as possible)</li>
+        <li>Your account of what happened</li>
+        <li>Links, screenshots, context or any other information that may be used to better understand and resolve the situation</li>
+        <li>Any other information you believe we should have</li>
+      </ul>
+      <h2 class="coc-sub-heading" id="_2-what-happens-next">2. What happens next</h2>
+      <p>When you have sent off your report, you will receive an email from us acknowledging that we have recieved your report: the first one will be an automatic reply that your report has arrived in our inbox, and within 24 hours one of our organisers will contact you personally. If we need more information from you, we then also let you know as soon as possible.</p>
+       <p><em>We will respond in the language (English or German) you used in your report.</em></p>
+      <p>Once we have a complete account of the incident(s), we will make a decision as to how to respond. Responses may include:</p>
+      <ul class="coc-list">
+        <li>Nothing (if we determine no violation occurred)</li>
+        <li>A private reprimand from the organisers to the individual(s) involved</li>
+        <li>A public reprimand</li>
+        <li>A permanent or temporary ban from some or all CSS Frankfurt spaces</li> 
+        <li>A request for a public or private apology</li>
+      </ul>
+      <p>We will respond within one week to the person who filed the report with either a resolution or an explanation of why the situation is not yet resolved.</p>
+      <h2 class="coc-sub-heading" id="_3-questions-or-concerns">3. Questions or concerns</h2>
+      <p>If you have any questions or complaints regarding how your report was handled, please contact us at <strong>report@cssfrankfurt.de</strong>, and we will review your concerns.</p>
+    </article>
+  </section>
+</template>
+
+
+<style lang="sass">
+
+  .section-coc
+    max-width: 800px
+  
+  .coc-sub-heading
+    margin-top: 1rem
+  
+  .coc-list
+    padding-left: 3rem
+
+</style>
