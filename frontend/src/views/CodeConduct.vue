d0dedc426441c01399c824e9d91bd0b8458c5f16
vicbergquist <victoria.bergquist@gmail.com>
2018-10-12 13:38:08 +0200

    code of conduct

---
+++ frontend/src/views/CodeConduct.vue
@@ -0,0 +1,112 @@
+<template>
+  <section class="section section-coc">
+    <img 
+      src="/hacktoberfest_title.svg" 
+      alt="hacktoberfest" 
+      class="leaderboard-header">
+    <article class="article article-coc">
+      <h1 
+        id="code-of-conduct" 
+        class="coc-heading">Code of Conduct</h1>
+      <h2 
+        id="_1-purpose" 
+        class="coc-sub-heading">1. Purpose</h2> 
+      <p>A primary goal of Vue.js Frankfurt is to be inclusive to the largest number of community contributors, with the most varied and diverse backgrounds possible. As such, we are committed to providing a friendly, safe and welcoming environment for all, regardless of gender, sexual orientation, ability, ethnicity, socioeconomic status, and religion (or lack thereof).</p> <p>This code of conduct outlines our expectations for all those who participate in our community, as well as the consequences for unacceptable behavior.</p> <p>We invite all those who participate in Vue.js Frankfurt to help us create safe and positive experiences for everyone.</p> 
+      <h2 
+        id="_2-open-source-citizenship" 
+        class="coc-sub-heading">2. Open Source Citizenship</h2> 
+      <p>A supplemental goal of this Code of Conduct is to increase open source citizenship by encouraging participants to recognize and strengthen the relationships between our actions and their effects on our community.</p> 
+      <p>Communities mirror the societies in which they exist and positive action is essential to counteract the many forms of inequality and abuses of power that exist in society.</p> <p>If you see someone who is making an extra effort to ensure our community is welcoming, friendly, and encourages all participants to contribute to the fullest extent, we want to know.</p> 
+      <h2 
+        id="_3-expected-behavior" 
+        class="coc-sub-heading">3. Expected Behavior</h2>
+      <p>The following behaviors are expected and requested of all community members:</p> 
+      <ul class="coc-list">
+        <li>Participate in an authentic and active way. In doing so, you contribute to the health and longevity of this community.</li>
+        <li>Exercise consideration and respect in your speech and actions.</li> 
+        <li>Attempt collaboration before conflict.</li>
+        <li>Refrain from demeaning, discriminatory, or harassing behavior and speech.</li> <li>Be mindful of your surroundings and of your fellow participants. Alert community leaders if you notice a dangerous situation, someone in distress, or violations of this Code of Conduct, even if they seem inconsequential.</li> <li>Remember that community event venues may be shared with members of the public; please be respectful to all patrons of these locations.</li>
+      </ul>
+      <h2 
+        id="_4-unacceptable-behavior" 
+        class="coc-sub-heading">4. Unacceptable Behavior</h2>
+      <p>The following behaviors are considered harassment and are unacceptable within our community:</p>
+      <ul class="coc-list">
+        <li>Violence, threats of violence or violent language directed against another person.</li>
+        <li>Sexist, racist, homophobic, transphobic, ableist or otherwise discriminatory jokes and language.</li>
+        <li>Posting or displaying sexually explicit or violent material.</li>
+        <li>Posting or threatening to post other people’s personally identifying information (“doxing”).</li>
+        <li>Personal insults, particularly those related to gender, sexual orientation, race, religion, or disability.</li>
+        <li>Inappropriate photography or recording.</li>
+        <li>Inappropriate physical contact. You should have someone’s consent before touching them.</li>
+        <li>Unwelcome sexual attention. This includes, sexualized comments or jokes; inappropriate touching, groping, and unwelcomed sexual advances.</li>
+        <li>Deliberate intimidation, stalking or following (online or in person).</li>
+        <li>Advocating for, or encouraging, any of the above behavior.</li> 
+        <li>Sustained disruption of community events, including talks and presentations.</li>
+      </ul>
+      <h2 
+        id="_5-consequences-of-unacceptable-behavior" 
+        class="coc-sub-heading">5. Consequences of Unacceptable Behavior</h2>
+      <p>Unacceptable behavior from any community member, including sponsors and those with decision-making authority, will not be tolerated.</p>
+      <p>Anyone asked to stop unacceptable behavior is expected to comply immediately.</p>
+      <p>If a community member engages in unacceptable behavior, the community organizers may take any action they deem appropriate, up to and including a temporary ban or permanent expulsion from the community without warning (and without refund in the case of a paid event).</p>
+      <h2 
+        id="_6-reporting-guidelines" 
+        class="coc-sub-heading">6. Reporting Guidelines</h2>
+      <p>If you are subject to or witness unacceptable behavior, or have any other concerns, please notify a community organizer as soon as possible or send an email to <strong>hello@hacktoberfestffm.de</strong>. Please write your report according to our <router-link 
+        to="/rg" 
+        class="inline-link">guidelines</router-link>.</p>
+      <p>Additionally, community organizers are available to help community members engage with local law enforcement or to otherwise help those experiencing unacceptable behavior feel safe. In the context of in-person events, organizers will also provide escorts as desired by the person experiencing distress.</p>
+      <p>In order to protect volunteers from abuse and burnout, we reserve the right to reject any report we believe to have been made in bad faith. Reports intended to silence legitimate criticism may be deleted without response.</p>
+      <p>We will respect confidentiality requests for the purpose of protecting victims of abuse. At our discretion, we may publicly name a person about whom we’ve received harassment complaints, or privately warn third parties about them, if we believe that doing so will increase the safety of other community members. We will not name harassment victims without their affirmative consent.</p>
+      <h2 
+        id="_7-addressing-grievances" 
+        class="coc-sub-heading">7. Addressing Grievances</h2>
+      <p>If you feel you have been falsely or unfairly accused of violating this Code of Conduct, you should notify us with a concise description of your grievance. Your grievance will be handled in accordance with our existing governing policies.</p>
+      <h2 
+        id="_8-scope" 
+        class="coc-sub-heading">8. Scope</h2>
+      <p>We expect all community participants (contributors, paid or otherwise; sponsors; and other guests) to abide by this Code of Conduct in all community venues–online and in-person–as well as in all one-on-one communications pertaining to community business.</p>
+      <p>This code of conduct and its related procedures also applies to unacceptable behavior occurring outside the scope of community activities when such behavior has the potential to adversely affect the safety and well-being of community members.</p>
+      <h2 
+        id="_9-contact-info" 
+        class="coc-sub-heading">9. Contact info</h2>
+      <p>hello@hacktoberfestffm.de</p>
+      <h2 
+        id="_10-license-and-attribution" 
+        class="coc-sub-heading">10. License and attribution</h2>
+      <p>This Code of Conduct is distributed under a <a 
+        href="http://creativecommons.org/licenses/by-sa/3.0/" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        class="inline-link">Creative Commons Attribution-ShareAlike license</a>. Portions of text derived from the <a 
+          class="inline-link" 
+          href="https://www.djangoproject.com/conduct/" 
+          target="_blank" 
+          rel="noopener noreferrer">Django Code of Conduct</a> and the <a 
+            href="http://geekfeminism.wikia.com/wiki/Conference_anti-harassment/Policy" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            class="inline-link">Geek Feminism Anti-Harassment Policy</a>. Retrieved on November 22, 2016 from <a 
+              href="http://citizencodeofconduct.org/" 
+              target="_blank" 
+              class="inline-link" 
+              rel="noopener noreferrer">http://citizencodeofconduct.org/</a>
+      </p>
+    </article>
+  </section>
+</template>
+
+
+<style lang="sass">
+
+  .section-coc
+    max-width: 800px
+  
+  .coc-sub-heading
+    margin-top: 1rem
+  
+  .coc-list
+    padding-left: 3rem
+
+</style>
