9d264d832c4eab28b30d76103f02115ccb2def64
Felix Wu <mail@flxwu.com>
2018-10-14 09:25:54 +0200

    set date break at first oct

--- routes/api.js
+++ routes/api.js
@@ -121,7 +121,7 @@ router.get('/data', async (req, res, next) => {
                 obj.type === 'PullRequestEvent' &&
                 obj.payload.action === 'opened' &&
                 new Date(obj.payload.pull_request.created_at) >
-                  new Date('2018-10-12T16:00:29.170Z')
+                  new Date('2018-10-01')
               ) {
                 prsPerUser[users[i].login] = {
                   latestPr:
